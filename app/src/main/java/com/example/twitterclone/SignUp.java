237a6b91ab95d98c10d34330f3c62b8fedf30698
Mostafa2293 <mostafamohamed05@gmail.com>
2019-12-15 15:10:18 +0200

    backEnd settings and handling the signingUp

--- app/src/main/java/com/example/twitterclone/SignUp.java
+++ app/src/main/java/com/example/twitterclone/SignUp.java
@@ -3,14 +3,17 @@ package com.example.twitterclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.parse.ParseException;
+import com.parse.ParseInstallation;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
@@ -30,18 +33,20 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_sign_up);
         setTitle(R.string.sign_up_activity_title);
 
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
         // init UI components
         edtEmail = findViewById(R.id.edtEditEmail);
         edtUsername = findViewById(R.id.edtUserName);
         edtPassword = findViewById(R.id.edtPassword);
         btnLogIn = findViewById(R.id.btnLogIn);
-        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp = findViewById(R.id.btnSignUpLoginActivity);
 
         edtPassword.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
 
-                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
 
                     onClick(btnSignUp);
 
@@ -55,23 +60,25 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         btnSignUp.setOnClickListener(this);
 
         if (ParseUser.getCurrentUser() != null) {
-             ParseUser.getCurrentUser().logOut();
-             transitionToSocialMediaActivity();
+            //ParseUser.getCurrentUser().logOut();
+            transitionToSocialMediaActivity();
         }
-
-        
 
 
     }
 
     private void transitionToSocialMediaActivity() {
+        Intent intent = new Intent(SignUp.this, TwitterUsers.class);
+        startActivity(intent);
+        finish();
+
     }
 
     @Override
     public void onClick(View view) {
 
-        switch (view.getId()){
-            case R.id.btnSignUp:
+        switch (view.getId()) {
+            case R.id.btnSignUpLoginActivity:
                 if (edtEmail.getText().toString().equals("") || edtPassword.getText().toString().equals("") || edtUsername.getText().toString().equals("")) {
 
                     FancyToast.makeText(SignUp.this, "Email, Username, Password Is Required", Toast.LENGTH_SHORT, FancyToast.INFO, true).show();
@@ -103,11 +110,26 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
                 break;
 
             case R.id.btnLogIn:
-
-                
-
+                Intent intent = new Intent(SignUp.this, LoginActivity.class);
+                startActivity(intent);
                 break;
         }
+
+    }
+
+    public void rootLayoutTapped(View view) {
+
+        try {
+
+            InputMethodManager inputMethodManager =
+                    (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
 
     }
 }
